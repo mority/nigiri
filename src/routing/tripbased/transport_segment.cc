8bbd0affe57b6a79b2582f5f7f625993fb18620f
mority <mority@posteo.net>
2023-06-20 18:45:15 +0200

    dbg logging, print stop indices

--- src/routing/tripbased/transport_segment.cc
+++ src/routing/tripbased/transport_segment.cc
@@ -8,14 +8,15 @@ void transport_segment::print(std::ostream& out, timetable const& tt) const {
   std::string transferred_from_str = transferred_from_ == TRANSFERRED_FROM_NULL
                                          ? "INIT"
                                          : std::to_string(transferred_from_);
-  out << tt.transport_name(get_transport_idx()) << " from "
+  out << tt.transport_name(get_transport_idx()) << " from stop "
+      << stop_idx_start_ << ": "
       << tt.locations_.names_
              .at(stop{tt.route_location_seq_
                           [tt.transport_route_[get_transport_idx()]]
                           [stop_idx_start_]}
                      .location_idx())
              .view()
-      << " to "
+      << " to stop " << stop_idx_end_ << ": "
       << tt.locations_.names_
              .at(stop{
                  tt.route_location_seq_
