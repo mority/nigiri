5ccacf23d3d1437a00ddf5ca2684f66d590b2bba
mority <mority@posteo.net>
2024-05-02 14:47:52 +0200

    wip

--- include/nigiri/routing/interval_estimate.h
+++ include/nigiri/routing/interval_estimate.h
@@ -9,32 +9,46 @@
 
 namespace nigiri::routing {
 
-constexpr auto const step = i32_minutes{60};
-constexpr auto const half_step = step / 2;
-
 template <direction SearchDir>
 struct interval_estimator {
   explicit interval_estimator(timetable const& tt, query const& q)
       : tt_{tt}, q_{q} {}
 
   interval<unixtime_t> initial(interval<unixtime_t> const& itv) const {
-    auto new_itv = itv;
-
     if (q_.min_connection_count_ == 0 ||
         (!q_.extend_interval_earlier_ && !q_.extend_interval_later_)) {
-      return new_itv;
+      return itv;
     }
 
-    // extend interval into allowed directions
-    if (can_extend_both_dir(new_itv)) {
-      new_itv.from_ -= half_step * q_.min_connection_count_;
-      new_itv.to_ += half_step * q_.min_connection_count_;
-    } else {
-      if (q_.extend_interval_earlier_) {
-        new_itv.from_ -= step * q_.min_connection_count_;
+    auto new_itv = itv;
+    auto const ext = 1_hours * q_.min_connection_count_;
+
+    // extend interval more into "bad" direction
+    if (can_extend_bad_dir(itv)) {
+      if constexpr (SearchDir == direction::kForward) {
+        if (can_extend_earlier(itv)) {
+          new_itv.from_ -= 1_hours;
+        } else {
+          new_itv.to_ += 1_hours;
+        }
+        new_itv.to_ += ext;
+      } else {
+        if (can_extend_later(itv)) {
+          new_itv.to_ += 1_hours;
+        } else {
+          new_itv.from_ -= 1_hours;
+        }
+        new_itv.from_ -= ext;
       }
-      if (q_.extend_interval_later_) {
-        new_itv.to_ += step * q_.min_connection_count_;
+    } else {
+      if constexpr (SearchDir == direction::kForward) {
+        if (q_.extend_interval_earlier_) {
+          new_itv.from_ -= 1_hours + ext;
+        }
+      } else {
+        if (q_.extend_interval_later_) {
+          new_itv.to_ += 1_hours + ext;
+        }
       }
     }
 
@@ -47,11 +61,16 @@ struct interval_estimator {
 
   interval<unixtime_t> extension(interval<unixtime_t> const& itv,
                                  std::uint32_t const num_con_req) const {
+    if (num_con_req == 0 ||
+        (!q_.extend_interval_earlier_ && !q_.extend_interval_later_)) {
+      return itv;
+    }
+
     auto new_itv = itv;
     auto const ext = itv.size() * num_con_req;
 
     // extend
-    if (can_extend_both_dir(new_itv)) {
+    if (can_extend_both_dir(itv)) {
       new_itv.from_ -= ext / 2;
       new_itv.to_ += ext / 2;
     } else {
@@ -71,10 +90,25 @@ struct interval_estimator {
   }
 
 private:
-  bool can_extend_both_dir(interval<unixtime_t> const& itv) const {
+  bool can_extend_earlier(interval<unixtime_t> const& itv) const {
     return q_.extend_interval_earlier_ &&
-           itv.from_ != tt_.external_interval().from_ &&
-           q_.extend_interval_later_ && itv.to_ != tt_.external_interval().to_;
+           itv.from_ != tt_.external_interval().from_;
+  }
+
+  bool can_extend_later(interval<unixtime_t> const& itv) const {
+    return q_.extend_interval_later_ && itv.to_ != tt_.external_interval().to_;
+  }
+
+  bool can_extend_bad_dir(interval<unixtime_t> const& itv) const {
+    if constexpr (SearchDir == direction::kForward) {
+      return can_extend_later(itv);
+    } else {
+      return can_extend_earlier(itv);
+    }
+  }
+
+  bool can_extend_both_dir(interval<unixtime_t> const& itv) const {
+    return can_extend_earlier(itv) && can_extend_later(itv);
   }
 
   timetable const& tt_;
