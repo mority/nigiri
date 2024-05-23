496046bddf49314e7cf652d64cb43771da58f79c
mority <mority@posteo.net>
2024-05-23 05:00:37 +0200

    Deducting an additional safety margin of 2 days in limits.h seems to prevent the overflow

--- include/nigiri/routing/limits.h
+++ include/nigiri/routing/limits.h
@@ -10,6 +10,6 @@ static constexpr auto const kMaxTransfers = std::uint8_t{15U};
 static constexpr auto const kMaxTravelTime = 5_days;
 static constexpr auto const kMaxSearchIntervalSize =
     date::days{std::numeric_limits<duration_t::rep>::max() / 1440} -
-    (kMaxTravelTime + 1_days);
+    (kMaxTravelTime + 2_days);
 
 }  // namespace nigiri::routing
