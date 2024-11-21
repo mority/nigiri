673a06a5f14f43081637ef9c23a4fb2a069db157
mority <mority@posteo.net>
2024-11-21 11:26:37 +0100

    wip

--- test/routing/raptor_n_to_all_test.cc
+++ test/routing/raptor_n_to_all_test.cc
@@ -2,6 +2,7 @@
 
 #include "nigiri/loader/gtfs/load_timetable.h"
 #include "nigiri/loader/init_finish.h"
+#include "nigiri/special_stations.h"
 
 #include "../raptor_search.h"
 
@@ -10,17 +11,21 @@ using namespace nigiri;
 using namespace nigiri::loader;
 using nigiri::test::raptor_n_to_all_search;
 
-void print(timetable const& tt,
+void print(std::ostream& out,
+           timetable const& tt,
            std::vector<std::vector<unixtime_t>> const& times) {
-  for (auto l = 0U; l != tt.n_locations() && l != times.size(); ++l) {
-    std::cout << tt.locations_.get(location_idx_t{l}).name_ << ":\n";
+  out << "\n";
+  for (auto l = special_stations_names.size();
+       l != tt.n_locations() && l != times.size(); ++l) {
+    out << tt.locations_.get(location_idx_t{l}).name_ << ":";
     for (auto i = 0U; i != times[l].size(); ++i) {
       if (times[l][i] != unixtime_t{duration_t{0}}) {
-        std::cout << i << ": " << times[l][i] << "  ";
+        out << " (#rides: " << i << ", " << times[l][i] << ")";
       }
     }
-    std::cout << "\n";
+    out << "\n";
   }
+  out << "\n";
 }
 
 mem_dir n_to_all_files() {
@@ -48,10 +53,12 @@ C2,C2,,,,,,,
 
 # routes.txt
 route_id,agency_id,route_short_name,route_long_name,route_desc,route_type
-A,MTA,A,A,A0 -> A2,0
-B,MTA,B,B,B0 -> B2,0
-C,MTA,C,C,C0 -> C2,0
-ABC,MTA,ABC,ABC,ABC,0
+A,MTA,A,A,A0 -> A1 -> A2,0
+B,MTA,B,B,B0 -> B1 -> B2,0
+C,MTA,C,C,C0 -> C1 -> C2,0
+ABC,MTA,ABC,ABC,A1 -> B1 -> C1,0
+BC,MTA,BC,BC,B2 -> C1,0
+CC,MTA,CC,CC,C0 -> C2,0
 
 # trips.txt
 route_id,service_id,trip_id,trip_headsign,block_id
@@ -59,6 +66,8 @@ A,D,A_TRIP,A_TRIP,1
 B,D,B_TRIP,B_TRIP,2
 C,D,C_TRIP,C_TRIP,3
 ABC,D,ABC_TRIP,ABC_TRIP,4
+BC,D,BC_TRIP,BC_TRIP,5
+CC,D,CC_TRIP,CC_TRIP,6
 
 # stop_times.txt
 trip_id,arrival_time,departure_time,stop_id,stop_sequence,pickup_type,drop_off_type
@@ -67,13 +76,17 @@ A_TRIP,09:00,10:00,A1,1,0,0
 A_TRIP,11:00,11:00,A2,2,0,0
 B_TRIP,09:00,09:00,B0,0,0,0
 B_TRIP,10:00,12:00,B1,1,0,0
-B_TRIP,13:00,13:00,B2,2,0,0
+B_TRIP,12:10,12:10,B2,2,0,0
 C_TRIP,10:00,10:00,C0,0,0,0
 C_TRIP,11:00,14:00,C1,1,0,0
 C_TRIP,15:00,15:00,C2,2,0,0
 ABC_TRIP,10:00,10:00,A1,0,0,0
 ABC_TRIP,11:00,12:00,B1,1,0,0
-ABC_TRIP,13:00,13:20,C1,2,0,0
+ABC_TRIP,13:00,13:00,C1,2,0,0
+BC_TRIP,12:20,12:20,B2,0,0,0
+BC_TRIP,12:30,12:30,C1,0,0,0
+CC_TRIP,11:00,11:00,C0,0,0,0
+CC_TRIP,12:00,12:00,C2,0,0,0
 
 )__");
 }
@@ -85,14 +98,24 @@ constexpr interval<std::chrono::sys_days> n_to_all_period() {
   return {from, to};
 }
 
-TEST(routing, one_to_all_forward) {
-  constexpr auto const src = source_idx_t{0U};
-  auto const config = loader_config{};
+constexpr auto const one_to_all_fwd_times = R"(
+A0: (#rides: 0, 2024-06-08 06:00)
+A1: (#rides: 1, 2024-06-08 07:02)
+A2: (#rides: 1, 2024-06-08 09:02)
+B0:
+B1: (#rides: 2, 2024-06-08 09:02)
+B2: (#rides: 3, 2024-06-08 10:12)
+C0:
+C1: (#rides: 2, 2024-06-08 11:02) (#rides: 4, 2024-06-08 10:32)
+C2: (#rides: 3, 2024-06-08 13:02)
 
+)";
+
+TEST(routing, one_to_all_fwd) {
   timetable tt;
   tt.date_range_ = n_to_all_period();
   register_special_stations(tt);
-  gtfs::load_timetable(config, src, n_to_all_files(), tt);
+  gtfs::load_timetable(loader_config{}, source_idx_t{0U}, n_to_all_files(), tt);
   finalize(tt);
 
   auto const times =
@@ -101,7 +124,15 @@ TEST(routing, one_to_all_forward) {
                                       unixtime_t{sys_days{2024_y / June / 9}}},
                              direction::kForward);
 
-  print(tt, times);
+  std::stringstream ss;
+  print(ss, tt, times);
+  EXPECT_EQ(std::string_view{one_to_all_fwd_times}, ss.str());
 }
 
-TEST(routing, one_to_all_backward) {}
+TEST(routing, one_to_all_bwd) {
+  timetable tt;
+  tt.date_range_ = n_to_all_period();
+  register_special_stations(tt);
+  gtfs::load_timetable(loader_config{}, source_idx_t{0U}, n_to_all_files(), tt);
+  finalize(tt);
+}
