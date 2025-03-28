c4984b01577cbe85ca0ee7b162ca2ad24c6598ee
mority <mority@posteo.net>
2025-03-28 14:43:17 +0100

    filter threshold

--- src/repertoire_filter.cc
+++ src/repertoire_filter.cc
@@ -1,14 +1,18 @@
 #include "nigiri/repertoire_filter.h"
 
+#include "utl/enumerate.h"
+
 namespace nigiri {
 
 namespace n = nigiri;
+
+constexpr auto const kFilterThreshold = 100U;
 
 void repertoire_filter(std::vector<n::location_idx_t> const& sorted_in,
                        std::vector<n::location_idx_t>& out,
                        n::timetable const& tt) {
   auto repertoire = bitvec{tt.n_routes()};
-  for (auto const& l : sorted_in) {
+  for (auto const [i, l] : utl::enumerate(sorted_in)) {
     auto expands_repertoire = false;
     for (auto const r : tt.location_routes_[l]) {
       if (!repertoire.test(r.v_)) {
@@ -16,7 +20,7 @@ void repertoire_filter(std::vector<n::location_idx_t> const& sorted_in,
       }
       repertoire.set(r.v_);
     }
-    if (expands_repertoire) {
+    if (i < kFilterThreshold || expands_repertoire) {
       out.emplace_back(l);
     }
   }
