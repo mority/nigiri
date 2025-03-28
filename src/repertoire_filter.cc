2b9b9f29cdcd6000b19a18eee33b83e87df39c23
mority <mority@posteo.net>
2025-03-28 16:21:17 +0100

    combine filter threshold and stations per route

--- src/repertoire_filter.cc
+++ src/repertoire_filter.cc
@@ -6,22 +6,23 @@ namespace nigiri {
 
 namespace n = nigiri;
 
-constexpr auto const kStationsPerRoute = std::uint8_t{2U};
+constexpr auto const kFilterThreshold = 200U;
+constexpr auto const kStationsPerRoute = std::uint8_t{3U};
 
 void repertoire_filter(std::vector<n::location_idx_t> const& sorted_in,
                        std::vector<n::location_idx_t>& out,
                        n::timetable const& tt) {
   auto repertoire = std::vector<std::uint8_t>{};
   repertoire.resize(tt.n_routes());
-  for (auto const l : sorted_in) {
+  for (auto const [i, l] : utl::enumerate(sorted_in)) {
     auto expands_repertoire = false;
     for (auto const r : tt.location_routes_[l]) {
       if (repertoire[r.v_] < kStationsPerRoute) {
         expands_repertoire = true;
+        ++repertoire[r.v_];
       }
-      ++repertoire[r.v_];
     }
-    if (expands_repertoire) {
+    if (i < kFilterThreshold || expands_repertoire) {
       out.emplace_back(l);
     }
   }
