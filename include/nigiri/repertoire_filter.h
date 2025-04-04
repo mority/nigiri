0d26ac5f02c66861a2dd215354a08c4a74eace7f
mority <mority@posteo.net>
2025-04-05 01:30:28 +0200

    filter anything with associated location

--- include/nigiri/repertoire_filter.h
+++ include/nigiri/repertoire_filter.h
@@ -7,9 +7,26 @@
 
 namespace nigiri {
 
-void repertoire_filter(std::vector<nigiri::location_idx_t>& sorted_in,
-                       std::vector<nigiri::location_idx_t>& out,
-                       nigiri::timetable const&,
-                       std::uint8_t stations_per_route);
+template <typename T>
+void repertoire_filter(std::vector<T>& sorted_in,
+                       std::vector<T>& sorted_out,
+                       std::function<location_idx_t(const T&)> get_loc,
+                       nigiri::timetable const& tt,
+                       std::uint8_t stations_per_route) {
+  auto repertoire = std::vector<std::uint8_t>{};
+  repertoire.resize(tt.n_routes());
+  for (auto const e : sorted_in) {
+    auto expands_repertoire = false;
+    for (auto const r : tt.location_routes_[get_loc(e)]) {
+      if (repertoire[r.v_] < stations_per_route) {
+        expands_repertoire = true;
+        ++repertoire[r.v_];
+      }
+    }
+    if (expands_repertoire) {
+      sorted_out.emplace_back(e);
+    }
+  }
+}
 
 }  // namespace nigiri
