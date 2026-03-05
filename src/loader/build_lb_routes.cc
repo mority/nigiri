57af9380b6c012ee38a80c0a7190410df755da8f
mority <mority@posteo.net>
2026-03-05 19:40:38 +0100

    wip

--- src/loader/build_lb_routes.cc
+++ src/loader/build_lb_routes.cc
@@ -1,9 +1,12 @@
 #include "nigiri/loader/build_lb_routes.h"
 
+#include "boost/geometry/algorithms/detail/envelope/interface.hpp"
+
 #include <unordered_set>
 
-#include "utl/enumerate.h"
 #include "utl/helpers/algorithm.h"
+#include "utl/progress_tracker.h"
+#include "utl/zip.h"
 
 namespace nigiri::loader {
 
@@ -11,12 +14,31 @@ static constexpr auto kUnset =
     lb_route_idx_t{std::numeric_limits<lb_route_idx_t::value_t>::max()};
 
 void build_lb_routes(timetable& tt, profile_idx_t prf_idx) {
+  auto const timer = scoped_timer{"loader.build_lb_routes"};
+
   auto route_lb_route = vector_map<route_idx_t, lb_route_idx_t>{};
   route_lb_route.resize(tt.n_routes());
   utl::fill(route_lb_route, kUnset);
   auto equivalence = std::vector<route_idx_t>{};
   auto lb_segments_layovers = std::vector<duration_t>{};
 
+  auto const equal_stops = [&](auto const r1, auto const r2) {
+    auto const& seq1 = tt.route_location_seq_[r1];
+    auto const& seq2 = tt.route_location_seq_[r2];
+    if (seq1.size() != seq2.size()) {
+      return false;
+    }
+    for (auto const [i, j] : utl::zip(seq1, seq2)) {
+      if (tt.locations_.get_root_idx(stop{i}.location_idx()) !=
+          tt.locations_.get_root_idx(stop{j}.location_idx())) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  auto const pt = utl::get_active_progress_tracker();
+  pt->status("Compute lower bound adjacencies").in_high(tt.n_locations());
   for (auto const representative :
        interval{route_idx_t{0U}, route_idx_t{tt.n_routes()}}) {
     if (route_lb_route[representative] != kUnset) {
@@ -30,8 +52,7 @@ void build_lb_routes(timetable& tt, profile_idx_t prf_idx) {
       if (route_lb_route[r] != kUnset) {
         continue;
       }
-      if (utl::equal(tt.route_location_seq_[representative],
-                     tt.route_location_seq_[r])) {
+      if (equal_stops(representative, r)) {
         equivalence.push_back(r);
       }
     }
@@ -62,6 +83,7 @@ void build_lb_routes(timetable& tt, profile_idx_t prf_idx) {
         }
       }
       route_lb_route[e] = lb_route_idx_t{tt.lb_route_route_[prf_idx].size()};
+      pt->increment();
     }
 
     tt.lb_route_times_[prf_idx].emplace_back(lb_segments_layovers);
@@ -77,5 +99,9 @@ void build_lb_routes(timetable& tt, profile_idx_t prf_idx) {
     }
     tt.location_lb_routes_[prf_idx].emplace_back(lb_routes);
   }
+
+  log(log_lvl::info, "nigiri.loader.lb_routes",
+      "Merged {} routes into {} LB routes", tt.n_routes(),
+      tt.lb_route_route_[prf_idx].size());
 }
 }  // namespace nigiri::loader
